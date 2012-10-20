1bd745362e91aa980b6153b60d2f2212239727f1
WengCSYZ <WengCSYZ@gmail.com>
2012-10-20 03:51:52 +0000

    finish compute_fluxes function, since the result is the same as that from sequential C function without already_compute_fluxes conditions, and working on gravity

--- anuga_cuda/compute_fluxes/cf.py
+++ anuga_cuda/compute_fluxes/cf.py
@@ -1,3 +1,8 @@
+from math import *
+import numpy
+from anuga_cuda.merimbula_data.generate_domain import *
+domain = domain_create()
+
 def _rotate(q, n1, n2):
     q1 = q[1]
     q2 = q[2]
@@ -26,18 +31,19 @@ def _compute_speed(uh, h, epsilon, h0, limiting_threshold):
 def _flux_function_central(q_left, q_right, z_left, z_right,
         n1, n2, epsilon, h0, limiting_threshold, g, edgeflux, max_speed):
 
-    q_left_rotated = []
-    q_right_rotated = []
+    q_left_rotated = numpy.zeros(3,dtype=numpy.float64)
+    q_right_rotated = numpy.zeros(3, dtype=numpy.float64)
     
-    flux_right = []
-    flux_left = []
-
-    q_left_rotated.append( q_left[0] )
-    q_right_rotated.append( q_right[0] )
-    q_left_rotated.append( q_left[1] )
-    q_right_rotated.append( q_right[1] )
-    q_left_rotated.append( q_left[2] )
-    q_right_rotated.append( q_right[2] )
+    flux_right = numpy.zeros(3, dtype=numpy.float64)
+    flux_left = numpy.zeros(3, dtype=numpy.float64)
+    
+    
+    q_left_rotated[0] =  q_left[0] 
+    q_right_rotated[0] = q_right[0] 
+    q_left_rotated[1] = q_left[1] 
+    q_right_rotated[1] = q_right[1] 
+    q_left_rotated[2] = q_left[2] 
+    q_right_rotated[2] = q_right[2] 
 
     
     _rotate(q_left_rotated, n1, n2);
@@ -45,21 +51,21 @@ def _flux_function_central(q_left, q_right, z_left, z_right,
 
     z = 0.5 * (z_left + z_right)
 
-    w_left = q_left_rotated[0];
+    w_left = q_left_rotated[0]
     h_left = w_left - z;
-    uh_left = q_left_rotated[1];
+    uh_left = q_left_rotated[1]
     u_left = _compute_speed( uh_left, h_left,
             epsilon, h0, limiting_threshold);
 
-    w_right = q_right_rotated[0];
+    w_right = q_right_rotated[0]
     h_right = w_right - z;
-    uh_right = q_right_rotated[1];
+    uh_right = q_right_rotated[1]
     u_right = _compute_speed( uh_right, h_right,
             epsilon, h0, limiting_threshold);
 
     
-    vh_left = q_left_rotated[2];
-    vh_right = q_right_rotated[2];
+    vh_left = q_left_rotated[2]
+    vh_right = q_right_rotated[2]
 
     
     
@@ -83,13 +89,13 @@ def _flux_function_central(q_left, q_right, z_left, z_right,
     if s_min > 0.0:
         s_min = 0.0;
 
-    flux_left[0] = u_left*h_left;
-    flux_left[1] = u_left * uh_left + 0.5 * g * h_left*h_left;
-    flux_left[2] = u_left*vh_left;
+    flux_left[0] = u_left*h_left
+    flux_left[1] = u_left * uh_left + 0.5 * g * h_left*h_left
+    flux_left[2] = u_left*vh_left
 
-    flux_right[0] = u_right*h_right;
-    flux_right[1] = u_right * uh_right + 0.5 * g * h_right*h_right;
-    flux_right[2] = u_right*vh_right;
+    flux_right[0] = u_right*h_right
+    flux_right[1] = u_right * uh_right + 0.5 * g * h_right*h_right
+    flux_right[2] = u_right*vh_right
 
     
     denom = s_max - s_min;
@@ -114,39 +120,60 @@ def _flux_function_central(q_left, q_right, z_left, z_right,
     
 
 def cf(domain, k , i):
+    
+    domain.quantities['stage'].explicit_update[k] = 0
+    domain.quantities['xmomentum'].explicit_update[k] = 0
+    domain.quantities['ymomentum'].explicit_update = 0
 
-    ql = []
-    qr = []
-    ql.append(domain.quantities['stage'].edge_values[k][i])
-    ql.append(domain.quantities['xmomentum'].edge_values[k][i])
-    ql.append(domain.quantities['ymomentum'].edge_values[k][i])
+    max_speed = 0
+    edgeflux = numpy.zeros(3, dtype=numpy.float64)
+
+    ql = numpy.zeros(3, dtype=numpy.float64)
+    qr = numpy.zeros(3, dtype=numpy.float64)
+    ql[0] = domain.quantities['stage'].edge_values[k][i]
+    ql[1] = domain.quantities['xmomentum'].edge_values[k][i]
+    ql[2] = domain.quantities['ymomentum'].edge_values[k][i]
     zl = domain.quantities['elevation'].edge_values[k][i]
 
     n = domain.neighbours[k][i]
 
     if n < 0 :
         m = -n -1;
-        qr.append(domain.quantities['stage'].boundary_values[m] )
-        qr.append(domain.quantities['xmomentum'].boundary_values[m] )
-        qr.append(domain.quantities['ymomentum'].boundary_values[m] )
+        qr[0] = domain.quantities['stage'].boundary_values[m] 
+        qr[1] = domain.quantities['xmomentum'].boundary_values[m] 
+        qr[2] = domain.quantities['ymomentum'].boundary_values[m] 
         zr = zl
     else:
         m = domain.neighbour_edges[k][i]
         
-        qr.append(domain.quantities['stage'].edge_values[n][m] )
-        qr.append(domain.quantities['xmomentum'].edge_values[n][m] )
-        qr.append(domain.quantities['ymomentum'].edge_values[n][m] )
+        qr[0] = domain.quantities['stage'].edge_values[n][m]
+        qr[1] = domain.quantities['xmomentum'].edge_values[n][m] 
+        qr[2] = domain.quantities['ymomentum'].edge_values[n][m] 
         zr = domain.quantities['elevation'].edge_values[n][m]
 
     if( domain.optimise_dry_cells):
-        if fabs(ql[0] - zl) < domain.epsilon and fabs(qr[0]-zr) < domain.epsilon:
+        if abs(ql[0] - zl) < domain.epsilon and abs(qr[0]-zr) < domain.epsilon:
             print "dry!!!!!!!"
             return
 
     _flux_function_central(ql, qr, zl, zr,
-                    domain.normals[ki2], domain.normals[ki2 + 1],
-                    domain.epsilon, domain.H0 * domain.H0, limiting_threshold, domain.g,
+                    domain.normals[k][i*2], domain.normals[k][i*2 + 1],
+                    domain.epsilon, domain.H0 * domain.H0, domain.H0 * 10, domain.g,
                     edgeflux, max_speed)
 
+    inv_area = 1.0/ domain.areas[k]
 
+    edgeflux[0] *= domain.edgelengths[k][i] * inv_area
+    edgeflux[1] *= domain.edgelengths[k][i] * inv_area
+    edgeflux[2] *= domain.edgelengths[k][i] * inv_area
 
+    
+
+    print edgeflux
+    return edgeflux
+
+if __name__ == '__main__':
+    #from anuga_cuda.merimbula_data.generate_domain import *
+    #domain = domain_create()
+    #cf(domain, 70,2)
+    pass
