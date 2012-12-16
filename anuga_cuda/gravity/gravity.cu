a5dcd83011ffd3c2b034f3fec2bbd68d4866cfef
WengCSYZ <WengCSYZ@gmail.com>
2012-12-16 10:52:09 +0000

    gravity with shared mem

---
+++ anuga_cuda/gravity/gravity.cu
@@ -0,0 +1,78 @@
+__global__ void gravity_wb(
+        double * stage_vertex_values, 
+        double * stage_edge_values, 
+        double * stage_centroid_values, 
+        double * bed_edge_values, 
+        double * bed_centroid_values, 
+        double * vertex_coordinates, 
+        double * xmom_explicit_update, 
+        double * ymom_explicit_update, 
+        double * normals, 
+        double * areas, 
+        double * edgelengths,
+        double g)
+{
+    const int k = threadIdx.x + (blockIdx.x )*blockDim.x;
+
+    int i;
+
+    double w0, w1, w2, 
+           x0, y0, x1, y1, x2, y2,
+           avg_h;
+
+    double wx, wy, det,
+           hh[3];
+    double area, n0, n1, fact;
+
+    __shared__ double sh_data[32*6];
+
+
+    w0 = stage_vertex_values[3*k + 0];
+    w1 = stage_vertex_values[3*k + 1];
+    w2 = stage_vertex_values[3*k + 2];
+
+    x0 = vertex_coordinates[k*6 + 0];
+    y0 = vertex_coordinates[k*6 + 1];
+    x1 = vertex_coordinates[k*6 + 2];
+    y1 = vertex_coordinates[k*6 + 3];
+    x2 = vertex_coordinates[k*6 + 4];
+    y2 = vertex_coordinates[k*6 + 5];
+
+
+    //_gradient(x0, y0, x1, y1, x2, y2, w0, w1, w2, &wx, &wy);
+
+    det = (y2 - y0)*(x1 - x0) - (y1 - y0)*(x2 - x0);
+
+    wx = (y2 -y0)*(w1 - w0) - (y1 - y0)*(w2 -w0);
+    wx /= det;
+
+    wy = (x1 - x0)*(w2 - w0) - (x2 - x0)*(w1 -w0);
+    wy /= det;
+
+
+    avg_h = stage_centroid_values[k] - bed_centroid_values[k];
+
+    xmom_explicit_update[k] += -g[0] * wx * avg_h;
+    ymom_explicit_update[k] += -g[0] * wy * avg_h;
+
+
+    hh[0] = stage_edge_values[k*3] - bed_edge_values[k*3];
+    hh[1] = stage_edge_values[k*3+1] - bed_edge_values[k*3+1];
+    hh[2] = stage_edge_values[k*3+2] - bed_edge_values[k*3+2];
+
+    area = areas[k];
+
+    for ( i = 0 ; i < 3 ; i++ )
+    {
+        n0 = normals[k*6 + 2*i];
+        n1 = normals[k*6 + 2*i + 1];
+
+        fact =  -0.5 * g[0] * hh[i] * hh[i] * edgelengths[k*3 + i];
+
+        sh_data[threadIdx.x + i*blockDim.x] = fact*n0;
+        sh_data[threadIdx.x + (i+3)*blockDim.x] = fact*n1;
+    }
+
+    xmom_explicit_update[k] += -(sh_data[threadIdx.x]+sh_data[threadIdx.x+blockDim.x]+sh_data[threadIdx.x+2*blockDim.x]) / area;
+    ymom_explicit_update[k] += -(sh_data[threadIdx.x+3*blockDim.x]+sh_data[threadIdx.x+4*blockDim.x]+sh_data[threadIdx.x+5*blockDim.x]) / area;
+}
