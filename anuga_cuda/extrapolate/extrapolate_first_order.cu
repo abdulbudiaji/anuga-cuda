8a98152e267cc57fb74d6c7ae234dc4297f41f4d
WengCSYZ <WengCSYZ@gmail.com>
2013-01-21 00:29:06 +0000

    Borogove5: fix extrapolate first order function since edge and vertex values has 3 elements

--- anuga_cuda/extrapolate/extrapolate_first_order.cu
+++ anuga_cuda/extrapolate/extrapolate_first_order.cu
@@ -11,9 +11,11 @@ __global__ void extrapolate_first_order(
     if (k >= N)
         return;
 
-    edge_values[k*2] = centroid_values[k];
-    edge_values[k*2+ 1] = centroid_values[k];
+    edge_values[k*3] = centroid_values[k];
+    edge_values[k*3+ 1] = centroid_values[k];
+    edge_values[k*3+ 2] = centroid_values[k];
 
-    vertex_values[k*2] = centroid_values[k];
-    vertex_values[k*2+ 1] = centroid_values[k];
+    vertex_values[k*3] = centroid_values[k];
+    vertex_values[k*3+ 1] = centroid_values[k];
+    vertex_values[k*3+ 2] = centroid_values[k];
 }
