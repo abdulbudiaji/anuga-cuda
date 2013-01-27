6ee38723ce42fe7d5e911a35d2fd364fa45102f6
WengCSYZ@gmail.com <WengCSYZ@gmail.com@9f922120-d8f3-4f77-5172-dc7104b5279a>
2013-01-27 05:15:41 +0000

    Borogove5: fix update_centroids_of_velocities_and_height function, the order of input parameters does not agree on, also the dimension of centroid_value (43200 in merimbula) is not as same as boundary_values (1072)

--- trunk/anuga_cuda/update_centroids_of_velocities_and_height/update_centroids_of_velocities_and_height.cu
+++ trunk/anuga_cuda/update_centroids_of_velocities_and_height/update_centroids_of_velocities_and_height.cu
@@ -1,20 +1,22 @@
 __global__ void update_centroids_of_velocities_and_height(
-        int N,
-        double * w_C,
-        double * z_C,
-        double * uh_C,
-        double * vh_C,
-        double * u_C,
-        double * v_C,
-        double * h_C,
+        int N_c,
+        int N_b,
+        double * w_C, // stage_centroid_values
+        double * uh_C,// xmomentum_centroid_values
+        double * vh_C,// ymomentum_centroid_values
+        double * h_C, // height_centroid_values
+        double * z_C, // elevation_centroid_values
+        double * u_C, // xvelocity_centroid_values
+        double * v_C, // yvelocity_centroid_values
 
-        double * w_B,
-        double * z_B,
-        double * uh_B,
-        double * vh_B,
-        double * u_B,
-        double * v_B,
-        double * h_B)
+        double * w_B, // stage_boundary_values
+        double * uh_B,// xmomentum_boundary_values
+        double * vh_B,// ymomentum_boundary_values
+        double * h_B, // height_boundary_values
+        double * z_B, // elevation_boundary_values
+        double * u_B, // xvelocity_boundary_values
+        double * v_B // yvelocity_boundary_values
+        )
 {
     const int k = 
             threadIdx.x+threadIdx.y*blockDim.x+
@@ -23,21 +25,26 @@ __global__ void update_centroids_of_velocities_and_height(
     double H0 = 1.0e-8;
     double factor;
 
-    if (k >=N)
+    if ( k >=N_c )
         return;
 
     h_C[k] = w_C[k] - z_C[k];
-
     if (h_C[k] < 0)
         h_C[k] = 0;
 
-    h_B[k] = w_B[k] - z_B[k];
-    if (h_B[k] < 0)
-        h_B[k] = 0;
 
     factor = h_C[k] / (h_C[k]*h_C[k] + H0);
     u_C[k] = uh_C[k]*factor;
     v_C[k] = vh_C[k]*factor;
+
+
+
+    if (k >= N_b)
+        return;
+
+    h_B[k] = w_B[k] - z_B[k];
+    if (h_B[k] < 0)
+        h_B[k] = 0;
 
 
     factor = h_B[k] / (h_B[k]*h_B[k] + H0);
--- trunk/anuga_cuda/update_centroids_of_velocities_and_height/update_centroids_of_velocities_and_height.py
+++ trunk/anuga_cuda/update_centroids_of_velocities_and_height/update_centroids_of_velocities_and_height.py
@@ -1,48 +1,154 @@
+#!/usr/bin/env python
+
 
 import numpy
 from pycuda import driver as drv
-from anuga_cuda.merimbula_data.channel1 import generate_domain
-from anuga_cuda.merimbula_data.generate_domain import domain_create
+from anuga_cuda import generate_merimbula_domain
+from anuga_cuda import generate_cairns_domain
+from anuga_cuda import generate_channel3_domain
 
-#domain1 = generate_domain( gpu=False )
-#domain2 = generate_domain( gpu=True )
-domain1 = domain_create( gpu=False )
-domain2 = domain_create( gpu=True )
+using_tsunami_domain = True
 
-domain1.distribute_to_vertices_and_edges()
-domain2.distribute_to_vertices_and_edges()
+if using_tsunami_domain:
+    domain1 = generate_cairns_domain(False)
+    domain2 = generate_cairns_domain(True)
+else:
+    domain1 = generate_merimbula_domain(False)
+    domain2 = generate_merimbula_domain(True)
+
+    #domain1 = generate_channel3_domain()
+    #domain2 = generate_channel3_domain(gpu=True)
 
 
+domain1.evolve(yieldstep = 50, finaltime = 500)
+domain1.evolve(yieldstep = 50, finaltime = 500)
+domain1.evolve(yieldstep = 50, finaltime = 500)
     
-N = domain1.quantities['elevation'].boundary.shape[0]
+
+domain2.evolve(yieldstep = 50, finaltime = 500)
+domain2.evolve(yieldstep = 50, finaltime = 500)
+domain2.evolve(yieldstep = 50, finaltime = 500)
+
+N = domain2.number_of_elements
 W1 = 32
 W2 = 1
 W3 = 1
 
-domain1.update_centroids_of_velocities_and_height_func()
+print "Testing input"
+print "  stage centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['stage'].centroid_values,
+        domain2.quantities['stage'].centroid_values)
+print "  xmomentum centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['xmomentum'].centroid_values,
+        domain2.quantities['xmomentum'].centroid_values)
+print "  ymomentum centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['ymomentum'].centroid_values,
+        domain2.quantities['ymomentum'].centroid_values)
+print "  height centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['height'].centroid_values,
+        domain2.quantities['height'].centroid_values)
+print "  elevation centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['elevation'].centroid_values,
+        domain2.quantities['elevation'].centroid_values)
+print "  xvelocity centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['xvelocity'].centroid_values,
+        domain2.quantities['xvelocity'].centroid_values)
+print "  yvelocity centroid_values all close? ", numpy.allclose( 
+        domain1.quantities['yvelocity'].centroid_values,
+        domain2.quantities['yvelocity'].centroid_values)
+
+print "  stage boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['stage'].boundary_values,
+        domain2.quantities['stage'].boundary_values)
+print "  xmomentum boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['xmomentum'].boundary_values,
+        domain2.quantities['xmomentum'].boundary_values)
+print "  ymomentum boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['ymomentum'].boundary_values,
+        domain2.quantities['ymomentum'].boundary_values)
+print "  height boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['height'].boundary_values,
+        domain2.quantities['height'].boundary_values)
+print "  elevation boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['elevation'].boundary_values,
+        domain2.quantities['elevation'].boundary_values)
+print "  xvelocity boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['xvelocity'].boundary_values,
+        domain2.quantities['xvelocity'].boundary_values)
+print "  yvelocity boundary_values all close? ", numpy.allclose( 
+        domain1.quantities['yvelocity'].boundary_values,
+        domain2.quantities['yvelocity'].boundary_values)
+
+
+
+domain1.update_centroids_of_velocities_and_height()
 
 domain2.quantities['elevation'].set_boundary_values_from_edges()
+domain2.update_centroids_of_velocities_and_height_func(
+        numpy.int32(N),
+        numpy.int32(domain2.quantities['stage'].boundary_values.shape[0]),
 
-domain2.interpolate_from_vertices_to_edges_func(
-    numpy.int32( N ),
-    drv.In( domain2.quantities['stage'].centroid_values ),
-    drv.In( domain2.quantities['xmomentum'].centroid_values ),
-    drv.In( domain2.quantities['ymomentum'].centroid_values ),
-    drv.InOut( domain2.quantities['height'].centroid_values ),
-    drv.InOut( domain2.quantities['elevation'].centroid_values ),
-    drv.InOut( domain2.quantities['xvelocity'].centroid_values ),
-    drv.In( domain2.quantities['yvelocity'].centroid_values ),
+        drv.In( domain2.quantities['stage'].centroid_values ),
+        drv.In( domain2.quantities['xmomentum'].centroid_values ),
+        drv.In( domain2.quantities['ymomentum'].centroid_values ),
+        drv.InOut( domain2.quantities['height'].centroid_values ),
+        drv.In( domain2.quantities['elevation'].centroid_values ),
+        drv.InOut( domain2.quantities['xvelocity'].centroid_values ),
+        drv.InOut( domain2.quantities['yvelocity'].centroid_values ),
 
-    drv.In( domain2.quantities['stage'].boundary_values ),
-    drv.In( domain2.quantities['xmomentum'].boundary_values ),
-    drv.In( domain2.quantities['ymomentum'].boundary_values ),
-    drv.InOut( domain2.quantities['height'].boundary_values ),
-    drv.In( domain2.quantities['elevation'].boundary_values ),
-    drv.In( domain2.quantities['xvelocity'].boundary_values ),
-    drv.In( domain2.quantities['yvelocity'].boundary_values ),
+        drv.In( domain2.quantities['stage'].boundary_values ),
+        drv.In( domain2.quantities['xmomentum'].boundary_values ),
+        drv.In( domain2.quantities['ymomentum'].boundary_values ),
+        drv.InOut( domain2.quantities['height'].boundary_values ),
+        drv.In( domain2.quantities['elevation'].boundary_values ),
+        drv.InOut( domain2.quantities['xvelocity'].boundary_values ),
+        drv.InOut( domain2.quantities['yvelocity'].boundary_values ),
+        block = (W1, W2, W3),
+        grid=((N+W1*W2*W3-1)/(W1*W2*W3),1)
+        )
 
-    block = (W1, W2, W3),
-    grid = ( (N + W1*W2*W3 -1) / (W1*W2*W3), 1)
-    )
+hc1 = domain1.quantities['height'].centroid_values
+hb1 = domain1.quantities['height'].boundary_values
+uc1 = domain1.quantities['xmomentum'].centroid_values
+ub1 = domain1.quantities['xmomentum'].boundary_values
+vc1 = domain1.quantities['ymomentum'].centroid_values
+vb1 = domain1.quantities['ymomentum'].boundary_values
 
-print numpy.allclose(Q1.edge_values, Q2.edge_values)
+hc2 = domain2.quantities['height'].centroid_values
+hb2 = domain2.quantities['height'].boundary_values
+uc2 = domain2.quantities['xmomentum'].centroid_values
+ub2 = domain2.quantities['xmomentum'].boundary_values
+vc2 = domain2.quantities['ymomentum'].centroid_values
+vb2 = domain2.quantities['ymomentum'].boundary_values
+wb2 = domain2.quantities['stage'].boundary_values
+zb2 = domain2.quantities['elevation'].boundary_values
+
+
+res = []
+res.append( numpy.allclose( hc1, hc2) )
+res.append( numpy.allclose( hb1, hb2) )
+res.append( numpy.allclose( uc1, uc2) )
+res.append( numpy.allclose( ub1, ub2) )
+res.append( numpy.allclose( vc1, vc2) )
+res.append( numpy.allclose( vb1, vb2) )
+
+print "\n\nTesting output"
+print "  height centroid_values all closed? " ,res[0]
+print "  height boundary_values all closed? " ,res[1]
+print "  xmom centroid_values all closed? " ,res[2]
+print "  xmom boundary_values all closed? " ,res[3]
+print "  ymom centroid_values all closed? " ,res[4]
+print "  ymom boundary_values all closed? " ,res[5]
+
+if not res.count(True) == 6:
+    cnt_hb = 0
+    res = []
+    for i in range( domain1.quantities['stage'].boundary_values.shape[0] ):
+        if hb1[i] != hb2[i]:
+            cnt_hb += 1
+            res.append(hb2[i])
+            if cnt_hb <10:
+                print "# %d, %f, %f, wb=%f, zb=%f" % \
+                    (i, hb1[i], hb2[i], wb2[i], zb2[i])
+    print " Number of diff: is %d " % cnt_hb
+    #print res
