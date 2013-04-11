b2066ee99046e79527e7fabdb82cbf4c901c95d4
WengCSYZ <WengCSYZ@gmail.com>
2013-04-11 14:22:09 +0000

    Devres: fixed python-openhmpp linkage problem

--- anuga_cuda/openHMPP/new/test_hmpp.py
+++ anuga_cuda/openHMPP/new/test_hmpp.py
@@ -1,3 +1,10 @@
+import ctypes
+import sys
+
+flags = sys.getdlopenflags()
+sys.setdlopenflags(flags | ctypes.RTLD_GLOBAL)
+
 from hmpp_python_glue import hmpp_python_test
+sys.setdlopenflags(flags)
 
 hmpp_python_test()
