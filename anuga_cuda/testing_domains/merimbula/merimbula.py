6031144f29b3b020c5b27bf592a0e969b2c39f9e
WengCSYZ <WengCSYZ@gmail.com>
2013-05-21 03:20:13 +0000

    Borogove5: add profile result on rg

--- anuga_cuda/testing_domains/merimbula/merimbula.py
+++ anuga_cuda/testing_domains/merimbula/merimbula.py
@@ -47,8 +47,8 @@ mesh_filename = "merimbula_43200.tsh"   ; x0 = 756000.0 ; x1 = 756500.0
 #mesh_filename = "test-100.tsh" ; x0 = 0.25 ; x1 = 0.5
 #mesh_filename = "test-20.tsh" ; x0 = 250.0 ; x1 = 350.0
 mesh_filename = merimbula_dir + mesh_filename
-yieldstep = 50
-finaltime = 50
+yieldstep = 10
+finaltime = 10
 verbose = True
 
 #--------------------------------------------------------------------------
