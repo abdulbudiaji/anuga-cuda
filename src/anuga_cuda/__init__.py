a97224b85c6cd06f842143f94bef9a989ade7fa2
WengCSYZ <WengCSYZ@gmail.com>
2013-06-07 02:58:19 +0000

    B5: add utilities

--- src/anuga_cuda/__init__.py
+++ src/anuga_cuda/__init__.py
@@ -7,15 +7,7 @@ from anuga_cuda.gpu_domain_advanced import GPU_domain
 
 
 # Import utilities
-from anuga_cuda.testing_domains.sort_domain import *
-from anuga_cuda.testing_domains.utility import *
+from anuga_cuda.utilities.sort_domain import *
+from anuga_cuda.utilities.utility import *
 
 
-# Import Channel1 domain
-from anuga_cuda.testing_domains.channel1 import *
-# Import Channel3 domain
-from anuga_cuda.testing_domains.channel3 import *
-# Import Merimbula domain
-from anuga_cuda.testing_domains.merimbula.generate_domain import *
-# Import Cairns domain
-from anuga_cuda.testing_domains.cairns.runcairns import *
