c2a374b34d7178ffc1ffd2fde587201bb83779f5
WengCSYZ <WengCSYZ@gmail.com>
2013-06-10 10:17:56 +0000

    B5: remove dependented_packages

--- src/depended_packages/pycuda-2012.1/clean_compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -Rf build
-rm ./siteconf.py
-./configure.py --cuda-root=$CUDA_ROOT --prefix=user --no-use-shipped-boost --boost-inc-dir=/apps/boost/1.46.1/include/ --boost-lib-dir=/apps/boost/1.46.1/lib/ --cuda-inc-dir=/apps/cuda/4.2.9/include/ --boost-compiler=gcc44
