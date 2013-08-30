39709f9461fd0493c713744789bfb1e5226d7102
WengCSYZ <WengCSYZ@gmail.com>
2013-08-30 04:45:55 +0000

    B5: working on the relative debugging by using Python decorator

--- src/utilities/check_result.py
+++ src/utilities/check_result.py
@@ -10,27 +10,37 @@ name_list = ['max_speed',
                 'stage_edge', 'elevation_edge', 'xmom_edge', 'ymom_edge', 
                 'height_edge', 'xvelocity_edge', 'yvelocity_edge', 
                 
-                'stage_centroid', 'elevation_centroid', 'xmom_centroid', 'ymom_centroid', 
-                'height_centroid', 'xvelocity_centroid', 'yvelocity_centroid', 
+                'stage_centroid', 'elevation_centroid', 'xmom_centroid',
+                'ymom_centroid', 
+                'height_centroid', 'xvelocity_centroid', 
+                'yvelocity_centroid', 
                 'friction_centroid',
                 
-                'stage_vertex', 'elevation_vertex', 'xmom_vertex', 'ymom_vertex', 
+                'stage_vertex', 'elevation_vertex', 'xmom_vertex', 
+                'ymom_vertex', 
                 'height_vertex', 'xvelocity_vertex', 'yvelocity_vertex', 
                 
-                'stage_explicit_update', 'xmom_explicit_update', 'ymom_explicit_update', 
+                'stage_explicit_update', 'xmom_explicit_update', 
+                'ymom_explicit_update', 
                 'stage_semi_implicit_update', 'xmom_semi_implicit_update', 
                 'ymom_semi_implicit_update', 
 
-                'stage_x_gradient', 'elevation_x_gradient', 'xmom_x_gradient', 
-                'ymom_x_gradient', 'height_x_gradient', 'xvelocity_x_gradient', 
+                'stage_x_gradient', 'elevation_x_gradient', 
+                'xmom_x_gradient', 
+                'ymom_x_gradient', 'height_x_gradient', 
+                'xvelocity_x_gradient', 
                 'yvelocity_x_gradient', 
                 
-                'stage_y_gradient', 'elevation_y_gradient', 'xmom_y_gradient', 
-                'ymom_y_gradient', 'height_y_gradient', 'xvelocity_y_gradient', 
+                'stage_y_gradient', 'elevation_y_gradient', 
+                'xmom_y_gradient', 
+                'ymom_y_gradient', 'height_y_gradient', 
+                'xvelocity_y_gradient', 
                 'yvelocity_y_gradient', 
 
-                'stage_boundary', 'elevation_boundary', 'xmom_boundary', 'ymom_boundary', 
-                'height_boundary', 'xvelocity_boundary', 'yvelocity_boundary' 
+                'stage_boundary', 'elevation_boundary', 'xmom_boundary', 
+                'ymom_boundary', 
+                'height_boundary', 'xvelocity_boundary', 
+                'yvelocity_boundary' 
             ]
 
 
@@ -203,6 +213,133 @@ def check_all(domain, test_domain):
             test_domain.quantities['xvelocity'].boundary_values)
     check_result(domain.quantities['yvelocity'].boundary_values, 
             test_domain.quantities['yvelocity'].boundary_values)
+
+
+
+def check_all_with_copy_back(domain, test_domain):
+    """Check all the result"""
+
+    global global_cnt
+    global_cnt = 0
+    print domain.flux_timestep, test_domain.flux_timestep
+    
+    
+    check_result(domain, test_domain, "max_speed")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "edge_values")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "edge_values")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "edge_values")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "edge_values")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "edge_values")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "edge_values")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "edge_values")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "centroid_values")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "centroid_values")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "centroid_values")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "centroid_values")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "centroid_values")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "centroid_values")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "centroid_values")
+    check_result(domain.quantities['friction'], 
+            test_domain.quantities['friction'], "centroid_values")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "vertex_values")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "vertex_values")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "vertex_values")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "vertex_values")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "vertex_values")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "vertex_values")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "vertex_values")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "explicit_update")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "explicit_update")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "explicit_update")
+    
+        
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "semi_implicit_update")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "semi_implicit_update")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "semi_implicit_update")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "x_gradient")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "x_gradient")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "x_gradient")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "x_gradient")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "x_gradient")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "x_gradient")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "x_gradient")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "y_gradient")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "y_gradient")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "y_gradient")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "y_gradient")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "y_gradient")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "y_gradient")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "y_gradient")
+    
+    
+    check_result(domain.quantities['stage'], 
+            test_domain.quantities['stage'], "boundary_values")
+    check_result(domain.quantities['elevation'], 
+            test_domain.quantities['elevation'], "boundary_values")
+    check_result(domain.quantities['xmomentum'], 
+            test_domain.quantities['xmomentum'], "boundary_values")
+    check_result(domain.quantities['ymomentum'], 
+            test_domain.quantities['ymomentum'], "boundary_values")
+    check_result(domain.quantities['height'], 
+            test_domain.quantities['height'], "boundary_values")
+    check_result(domain.quantities['xvelocity'], 
+            test_domain.quantities['xvelocity'], "boundary_values")
+    check_result(domain.quantities['yvelocity'], 
+            test_domain.quantities['yvelocity'], "boundary_values")
+    
 
 
 
